cc8dca04c23d1fc30680eb31dda4c801d9d245ae
Krishna <krishna.tripathi@flipkart.com>
2019-05-18 14:08:42 +0530

    added call method along with content scripts to add an iframe on top of search

---
+++ Flipkart/anotherscript.js
@@ -0,0 +1,9 @@
+iframes = document.getElementsByTagName("iframe");
+
+
+for (iframe in iframes){
+    console.log(iframes[iframe].src);
+
+}
+//console.log(innerDoc.getElementById("test_flipkart"))
+console.log("In another Script");
